5a28afa547910d98a05149513400097bc47bc651
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-05-28 12:11:00 +0300

    update branch

--- test/hint.test.js
+++ test/hint.test.js
@@ -1,60 +1,61 @@
-var test = require('tap').test,
-    fs = require('fs'),
-    glob = require('glob'),
-    fuzzer = require('fuzzer'),
-    exec = require('child_process').exec,
-    path = require('path'),
-    geojsonhint = require('../');
+import ext_tap_tap from "tap";
+import ext_fs_fs from "fs";
+import ext_glob_glob from "glob";
+import ext_fuzzer_fuzzer from "fuzzer";
+import ext_child_process_child_process from "child_process";
+import ext_path_path from "path";
+import { hint as index_hintjs } from "../";
+var test = ext_tap_tap.test, exec = ext_child_process_child_process.exec;
 
 function file(x) {
-    return fs.readFileSync(x, 'utf8');
+    return ext_fs_fs.readFileSync(x, 'utf8');
 }
 
 function filejs(x) {
-    return JSON.parse(fs.readFileSync(x, 'utf8'));
+    return JSON.parse(ext_fs_fs.readFileSync(x, 'utf8'));
 }
 
 test('geojsonhint', function(t) {
-    glob.sync('test/data/good/*.geojson').forEach(function(f) {
+    ext_glob_glob.sync('test/data/good/*.geojson').forEach(function(f) {
         var gj = file(f);
-        t.deepEqual(geojsonhint.hint(gj), [], f);
+        t.deepEqual(index_hintjs(gj), [], f);
     });
-    t.deepEqual(geojsonhint.hint(undefined), [{
+    t.deepEqual(index_hintjs(undefined), [{
         message: 'Expected string or object as input',
         line: 0
     }], 'expected string input');
-    t.deepEqual(geojsonhint.hint('{}'), [{
+    t.deepEqual(index_hintjs('{}'), [{
         message: '"type" member required',
         line: 1
     }], 'just an object');
     test('validates incorrect files', function(t) {
-        glob.sync('test/data/bad/*.geojson').forEach(function(f) {
+        ext_glob_glob.sync('test/data/bad/*.geojson').forEach(function(f) {
             var gj = file(f);
             if (process.env.UPDATE) {
-                fs.writeFileSync(f.replace('geojson', 'result'), JSON.stringify(geojsonhint.hint(gj), null, 2));
+                ext_fs_fs.writeFileSync(f.replace('geojson', 'result'), JSON.stringify(index_hintjs(gj), null, 2));
             }
-            t.deepEqual(geojsonhint.hint(gj), filejs(f.replace('geojson', 'result')), f);
+            t.deepEqual(index_hintjs(gj), filejs(f.replace('geojson', 'result')), f);
         });
         t.end();
     });
     test('binary produces output for bad files', function(t) {
-        glob.sync('test/data/bad/*.geojson').slice(0, 10).forEach(function(f) {
+        ext_glob_glob.sync('test/data/bad/*.geojson').slice(0, 10).forEach(function(f) {
             t.test(f + ' pretty', function(tt) {
-                exec(path.join(__dirname, '../bin/geojsonhint ' + f), function(err, output) {
+                exec(ext_path_path.join(__dirname, '../bin/geojsonhint ' + f), function(err, output) {
                     if (process.env.UPDATE) {
-                        fs.writeFileSync(f.replace('geojson', 'cli-output-pretty'), output);
+                        ext_fs_fs.writeFileSync(f.replace('geojson', 'cli-output-pretty'), output);
                     }
-                    var expected = fs.readFileSync(f.replace('geojson', 'cli-output-pretty'), 'utf8');
+                    var expected = ext_fs_fs.readFileSync(f.replace('geojson', 'cli-output-pretty'), 'utf8');
                     tt.equal(output, expected);
                     tt.end();
                 });
             });
             t.test(f + ' json', function(tt) {
-                exec(path.join(__dirname, '../bin/geojsonhint ' + f + ' --format=json'), function(err, output) {
+                exec(ext_path_path.join(__dirname, '../bin/geojsonhint ' + f + ' --format=json'), function(err, output) {
                     if (process.env.UPDATE) {
-                        fs.writeFileSync(f.replace('geojson', 'cli-output-json'), output);
+                        ext_fs_fs.writeFileSync(f.replace('geojson', 'cli-output-json'), output);
                     }
-                    var expected = fs.readFileSync(f.replace('geojson', 'cli-output-json'), 'utf8');
+                    var expected = ext_fs_fs.readFileSync(f.replace('geojson', 'cli-output-json'), 'utf8');
                     tt.equal(output, expected);
                     tt.end();
                 });
@@ -63,24 +64,24 @@ test('geojsonhint', function(t) {
         t.end();
     });
     test('validates incorrect files as objects', function(t) {
-        glob.sync('test/data/bad/*.geojson').forEach(function(f) {
+        ext_glob_glob.sync('test/data/bad/*.geojson').forEach(function(f) {
             if (f === 'test/data/bad/bad-json.geojson') return;
             var gj = filejs(f);
             if (process.env.UPDATE) {
-                fs.writeFileSync(f.replace('geojson', 'result-object'), JSON.stringify(geojsonhint.hint(gj), null, 2));
+                ext_fs_fs.writeFileSync(f.replace('geojson', 'result-object'), JSON.stringify(index_hintjs(gj), null, 2));
             }
-            t.deepEqual(geojsonhint.hint(gj), filejs(f.replace('geojson', 'result-object')), f);
+            t.deepEqual(index_hintjs(gj), filejs(f.replace('geojson', 'result-object')), f);
         });
         t.end();
     });
     test('noDuplicateMembers option=false', function(t) {
-        t.deepEqual(geojsonhint.hint('{"type":"invalid","type":"Feature","properties":{},"geometry":null}', {
+        t.deepEqual(index_hintjs('{"type":"invalid","type":"Feature","properties":{},"geometry":null}', {
             noDuplicateMembers: false
         }), [], 'sketchy object permitted');
         t.end();
     });
     test('noDuplicateMembers option=true', function(t) {
-        t.deepEqual(geojsonhint.hint('{"type":"invalid","type":"Feature","properties":{},"geometry":null}', {
+        t.deepEqual(index_hintjs('{"type":"invalid","type":"Feature","properties":{},"geometry":null}', {
             noDuplicateMembers: true
         }), [{
           line: 1,
@@ -89,7 +90,7 @@ test('geojsonhint', function(t) {
         t.end();
     });
     test('noDuplicateMembers option=true', function(t) {
-        t.deepEqual(geojsonhint.hint('{ "type": "Point", "coordinates": [100.0000000001, 5.0000000001] }', {
+        t.deepEqual(index_hintjs('{ "type": "Point", "coordinates": [100.0000000001, 5.0000000001] }', {
             precisionWarning: true
         }), [{
           line: 1,
@@ -119,7 +120,7 @@ test('geojsonhint', function(t) {
             features: []
         };
         for (var i = 0; i < 100; i++) featureCollection.features.push(excessiveFeature);
-        var truncated = geojsonhint.hint(JSON.stringify(featureCollection, null, 2), {
+        var truncated = index_hintjs(JSON.stringify(featureCollection, null, 2), {
             precisionWarning: true
         })
         t.equal(truncated.length, 11);
@@ -132,26 +133,26 @@ test('geojsonhint', function(t) {
         t.end();
     });
     test('invalid roots', function(t) {
-        t.deepEqual(geojsonhint.hint('null'), [{
+        t.deepEqual(index_hintjs('null'), [{
             message: 'The root of a GeoJSON object must be an object.',
             line: 0
         }], 'non-object root');
-        t.deepEqual(geojsonhint.hint('1'), [{
+        t.deepEqual(index_hintjs('1'), [{
             message: 'The root of a GeoJSON object must be an object.',
             line: 0
         }], 'number root');
-        t.deepEqual(geojsonhint.hint('"string"'), [{
+        t.deepEqual(index_hintjs('"string"'), [{
             message: 'The root of a GeoJSON object must be an object.',
             line: 0
         }], 'string root');
         t.end();
     });
-    glob.sync('test/data/good/*.geojson').forEach(function(f) {
-        var mutator = fuzzer.mutate.object(filejs(f));
+    ext_glob_glob.sync('test/data/good/*.geojson').forEach(function(f) {
+        var mutator = ext_fuzzer_fuzzer.mutate.object(filejs(f));
         for (var i = 0; i < 100; i++) {
             try {
                 var input = mutator();
-                geojsonhint.hint(input);
+                index_hintjs(input);
             } catch(e) {
                 t.fail('exception on ' + JSON.stringify(input));
             }
